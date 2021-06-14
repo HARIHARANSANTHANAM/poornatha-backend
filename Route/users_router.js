a6bb5abff7e1e6a028a0183a298a23473bd43fa1
Hariharan <hari21032001@gmail.com>
2021-06-14 14:53:44 +0530

    forgetpwd is added

--- Route/users_router.js
+++ Route/users_router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const Router = express.Router();
-const { User_Signup, User_Signin } = require("../controller/users_controller");
+const { User_Signup, User_Signin,User_ForgetPassword,User_ResetPassword } = require("../controller/users_controller");
 const Account = require("../Model/users_model");
 Router.post("/Signup", User_Signup);
 Router.post("/Signin", User_Signin);
+Router.post("/ForgetPassword", User_ForgetPassword);
+Router.patch("/ResetPassword/:_id", User_ResetPassword);
 
 module.exports = Router;
